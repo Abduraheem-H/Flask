91e162c6cdb1db49ccfbe80af72793d32b027fb5
Abduraheem Hussein <abdurahim.muzemil@aait.edu.et>
2025-08-10 02:16:29 +0300

    Refactor item and store resources to improve response handling and validation using marshmallow module

--- resources/item.py
+++ resources/item.py
@@ -4,25 +4,21 @@ from flask.views import MethodView
 import uuid
 
 from db import items, stores
+from schema import ItemSchema, ItemUpdateSchema
 
 bp = Blueprint("items", __name__, description="Operations on items")
 
 
 @bp.route("/item")
 class ItemView(MethodView):
-    def get(self):
-        return {"items": list(items.values())}, 200
 
-    def post(self):
-        item_data = request.get_json()
-        if item_data["store_id"] not in stores:
-            abort(404, message="Store not found!")
-        if (
-            "name" not in item_data
-            or "price" not in item_data
-            or "store_id" not in item_data
-        ):
-            abort(400, message="Missing required fields!")
+    @bp.response(200, ItemSchema(many=True))
+    def get(self):
+        return items.values()
+
+    @bp.arguments(ItemSchema)
+    @bp.response(201, ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if (
                 item["name"] == item_data["name"]
@@ -32,16 +28,18 @@ class ItemView(MethodView):
         item_id = uuid.uuid4().hex
         new_item = {**item_data, "id": item_id}
         items[item_id] = new_item
-        return new_item, 201
+        return new_item
 
 
 @bp.route("/item/<string:item_id>")
 class ItemDetailView(MethodView):
+
+    @bp.response(200, ItemSchema)
     def get(self, item_id):
         item = items.get(item_id)
         if not item:
             abort(404, message="Item not found!")
-        return item, 200
+        return item
 
     def delete(self, item_id):
         try:
@@ -50,11 +48,12 @@ class ItemDetailView(MethodView):
         except KeyError:
             abort(404, message="Item not found!")
 
-    def put(self, item_id):
-        updated_item_data = request.get_json()
-        if item_id not in items:
+    @bp.arguments(ItemUpdateSchema)
+    @bp.response(200, ItemSchema)
+    def put(self, updated_item_data, item_id):
+        try:
+            item = items[item_id]
+            item.update(updated_item_data)
+            return item
+        except KeyError:
             abort(404, message="Item not found!")
-        if "name" not in updated_item_data or "price" not in updated_item_data:
-            abort(400, message="Missing required fields!")
-        items[item_id].update(updated_item_data)
-        return {"Updated Item": items[item_id]}, 200
--- resources/store.py
+++ resources/store.py
@@ -4,35 +4,39 @@ from flask.views import MethodView
 import uuid
 
 from db import items, stores
+from schema import StoreSchema
 
 bp = Blueprint("stores", __name__, description="Operations on stores")
 
 
 @bp.route("/store")
 class StoreView(MethodView):
-    def get(self):
-        return {"stores": list(stores.values())}, 200
 
-    def post(self):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(400, message="Missing required fields!")
+    @bp.response(200, StoreSchema(many=True))
+    def get(self):
+        return stores.values()
+
+    @bp.arguments(StoreSchema)
+    @bp.response(201, StoreSchema)
+    def post(self, store_data):
         for store in stores.values():
             if store["name"] == store_data["name"]:
                 abort(400, message="Store already exists!")
         store_id = uuid.uuid4().hex
         new_store = {**store_data, "id": store_id}
         stores[store_id] = new_store
-        return new_store, 201
+        return new_store
 
 
 @bp.route("/store/<string:store_id>")
 class StoreDetailView(MethodView):
+
+    @bp.response(200, StoreSchema)
     def get(self, store_id):
         store = stores.get(store_id)
         if not store:
             abort(404, message="Store not found!")
-        return store, 200
+        return store
 
     def delete(self, store_id):
         try:
